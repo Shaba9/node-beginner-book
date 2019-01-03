48ad9f96e2ecab4105c60430bdb24bb120906155
Shaba9 <shaba92@gmail.com>
2019-01-02 17:35:17 -0800

    server.js exported

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var http = require('http');
 
-http.createServer((request, response) => {
-  console.log('Request received.');
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello World');
-  response.end();
-}).listen(8888);
+function start() {
+  http.createServer((request, response) => {
+    console.log('Request received.');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+  }).listen(8888);
+  
+  console.log('Server has started.');
+}
 
-console.log('Server has started.');
+exports.start = start;

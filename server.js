2d3f338af6cd7732ebd37123cc4a19c4fa85b783
Shaba9 <shaba92@gmail.com>
2019-01-02 22:44:22 -0800

    request passes to handle. image saves to server and displays

--- server.js
+++ server.js
@@ -3,21 +3,9 @@ var url = require('url');
 
 function start(route, handle) {
   http.createServer((request, response) => {
-    var postData = '';
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received.');
-
-    request.setEncoding('utf8');
-
-    request.addListener('data', postDataChunk => {
-      postData += postDataChunk;
-      console.log(`Received POST data chuck '${postDataChunk}'.`);
-    });
-
-    request.addListener('end', () => {
-      route(handle, pathname, response, postData);
-    });
-
+    route(handle, pathname, response, request);
   }).listen(8888);
   
   console.log('Server has started.');
